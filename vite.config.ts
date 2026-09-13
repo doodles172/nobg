ca6bd95f561ec69d3d540f494c22539476d6be09
doodles172 <eamon@vantern.org>
2026-09-13 23:05:59 +0200

    feat: add commit hash and environment to the bottom left corner

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import { execSync } from "node:child_process";
+
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
+  },
+  define: {
+    __COMMIT_HASH__: JSON.stringify(commitHash),
   },
 });
